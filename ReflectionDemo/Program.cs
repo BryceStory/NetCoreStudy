7ab5261d49489975e4ba6dd7083a123b4573d443
lx448174650 <lx448174650@outlook.com>
2020-09-06 12:06:42 +0800

    update

---
+++ ReflectionDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReflectionTest();
+
+            Console.ReadLine();
+        }
+
+        public static void ReflectionTest()
+        {
+             TargetClass targetClass = new TargetClass();
+
+
+            PropertyInfo[] infos= targetClass.GetType().GetProperties();
+            foreach (var item in infos)
+            {
+                Console.WriteLine($"item name :{item.GetMethod}");
+            }
+
+           // Console.WriteLine($"{infos.GetFields().ToString()}   full name : {infos.FullName}");
+        }
+    }
+}
---
+++ ReflectionDemo/TargetClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo
+{
+    public class TargetClass
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public void GetSometing()
+        { 
+        
+        }   
+    }
+}
