957e7359c0b3bf850711e6fdb1178cd8171d4695
lx448174650 <lx448174650@outlook.com>
2020-09-19 11:51:02 +0800

    update

---
+++ DelegateDemo/Common.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateDemo
+{
+    public static class Common
+    {
+        public enum PeopleType
+        {
+            China,
+            American,
+            Japan
+        }
+
+        public static void SayHiChina(string name)
+        {
+            Console.WriteLine($"{name},你好呀");
+        }
+
+        public static void SayHiAmerican(string name)
+        {
+            Console.WriteLine($"{name},What's up");
+        }
+
+        public static void SayHiJapan(string name)
+        {
+            Console.WriteLine($"{name},KONIJIWA");
+        }
+
+    }
+}
---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static DelegateDemo.Common;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = PeopleType.China;
+
+            switch (type)
+            {
+                case PeopleType.China:
+                    SayHiChina("hah");
+                    break;
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
