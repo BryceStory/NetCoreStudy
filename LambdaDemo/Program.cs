10caf85cc207d5e23bd38a3e4b9f564ac868a4e2
lx448174650 <lx448174650@outlook.com>
2020-09-19 12:53:53 +0800

    master

---
+++ LambdaDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LambdaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is lambda Demo");
+
+
+            var a = "Bryce";
+            var age = 13;
+
+           // () => { Console.WriteLine($"{a}dada{name}"); };
+
+
+            Console.WriteLine();
+        }
+    }
+}
